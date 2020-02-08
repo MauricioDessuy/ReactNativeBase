fd1ccc6775d7d5c233315467bc2988fa12e16bf7
Maurício Dessuy <mauriciodessuy@hotmail.com>
2020-02-08 20:06:03 -0300

    learned about ecma6

--- ReactNativeBase/App.js
+++ ReactNativeBase/App.js
@@ -1,23 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Olá mundo!</Text>
-      <Text>Olá mundo!</Text>
-      <Text>Olá mundo!</Text>
-      <Text>Olá mundo!</Text>
-      <Text>Olá mundo!</Text>
-    </View>
-  );
+import { Text, View, StyleSheet, Button } from 'react-native';
+
+export default class PrimeiroProjeto extends Component {
+
+  somar(a, b) {
+    return a + b;
+  }
+
+  render() {
+    let nome = 'Mauricio';
+    return (
+      <View>
+        <Text>Olá mundo! { nome }</Text>
+        <Text>Olá mundo! { nome }</Text>
+        <Text>Olá mundo! { 5 + 3 }</Text>
+        <Text style={styles.texto}>Olá mundo!</Text>
+        <Text style={styles.texto}>{ this.somar(5, 11) }</Text>
+        <Button title="Aperte" onPress={() => {
+          alert("Clicado aaaa");
+        }} />
+
+      </View>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  texto : {
+    fontSize: 30
+  }
 });
