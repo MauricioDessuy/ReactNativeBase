ddcaa3ca1c9c596cbf06bab88f20c5514e32e118
Maurício Dessuy <mauriciodessuy@hotmail.com>
2020-02-08 20:21:13 -0300

    learned about props

--- ReactNativeBase/App.js
+++ ReactNativeBase/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, StyleSheet, Button, Image } from 'react-native';
 
 export default class PrimeiroProjeto extends Component {
 
@@ -10,6 +10,9 @@ export default class PrimeiroProjeto extends Component {
 
   render() {
     let nome = 'Mauricio';
+    let imagem = {uri: 'https://as2.ftcdn.net/jpg/00/59/03/51/500_F_59035104_XtkeD0GJgfGxf9Hiq8tXNibjhy42ZwCx.jpg'};
+    //let imagem = {uri: './assets/image.jpg'};
+
     return (
       <View>
         <Text>Olá mundo! { nome }</Text>
@@ -20,7 +23,8 @@ export default class PrimeiroProjeto extends Component {
         <Button title="Aperte" onPress={() => {
           alert("Clicado aaaa");
         }} />
-
+        <Text style={{ fontSize: 30, color: 'red', margin: 20}}>Texto aleatório</Text>
+        <Image source={imagem} style={{width : 300, height: 300}}/>
       </View>
     );
   }
