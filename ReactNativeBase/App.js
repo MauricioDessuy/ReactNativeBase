7f7a5158d0257b86f1eed1e19fe1ff81a34329b6
Maurício Dessuy <mauriciodessuy@hotmail.com>
2020-02-08 20:49:12 -0300

    learned about states

--- ReactNativeBase/App.js
+++ ReactNativeBase/App.js
@@ -2,41 +2,41 @@ import React, { Component } from 'react';
 
 import { Text, View, StyleSheet, Button, Image } from 'react-native';
 
-class Imagem extends Component {
+class Janta extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      comida: props.comida
+    };
+    var comidas = ['Pizza', 'Lasanha', 'Burguer', 'Sopa', 'Arroz'];
+
+    setInterval(() => {
+      this.setState(previousState => {
+        let n = Math.floor(Math.random() * comidas.length);
+        return {comida: comidas[n]};
+      });
+    }, 1000);
+  }
+
   render() {
-    let imagem = {
-      uri: 'https://as2.ftcdn.net/jpg/00/59/03/51/500_F_59035104_XtkeD0GJgfGxf9Hiq8tXNibjhy42ZwCx' + this.props.nome
-    }
 
     return (
-      <Image source={imagem} style={{width: parseInt(this.props.largura), height: parseInt(this.props.altura)}}/>
+      <View>
+        <Text style={{textAlign:'center', fontWeight: 'bold', fontSize: 20, color: 'red'}}>Hoje você vai jantar: </Text>
+        <Text style={{textAlign: 'center', fontSize: 20}}>{this.state.comida}</Text>
+      </View>
     );
   }
 }
 
 export default class PrimeiroProjeto extends Component {
 
-  somar(a, b) {
-    return a + b;
-  }
-
   render() {
-    let nome = 'Mauricio';
-    let imagem = {uri: 'https://as2.ftcdn.net/jpg/00/59/03/51/500_F_59035104_XtkeD0GJgfGxf9Hiq8tXNibjhy42ZwCx.jpg'};
-    //let imagem = {uri: './assets/image.jpg'};
 
     return (
-      <View>
-        <Text>Olá mundo! { nome }</Text>
-        <Text>Olá mundo! { nome }</Text>
-        <Text>Olá mundo! { 5 + 3 }</Text>
-        <Text style={styles.texto}>Olá mundo!</Text>
-        <Text style={styles.texto}>{ this.somar(5, 11) }</Text>
-        <Button title="Aperte" onPress={() => {
-          alert("Clicado aaaa");
-        }} />
-        <Text style={{ fontSize: 30, color: 'red', margin: 20}}>Texto aleatório</Text>
-        <Imagem nome='.jpg' largura='250' altura='250'/>
+      <View style={{paddingTop: 30}}>
+        <Janta comida='Bolacha'/>
       </View>
     );
   }
