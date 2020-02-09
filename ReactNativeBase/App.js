c674553cddada83983820f734840c9beff795f2e
Maurício Dessuy <mauriciodessuy@hotmail.com>
2020-02-08 21:01:14 -0300

    learned about style groups

--- ReactNativeBase/App.js
+++ ReactNativeBase/App.js
@@ -2,41 +2,16 @@ import React, { Component } from 'react';
 
 import { Text, View, StyleSheet, Button, Image } from 'react-native';
 
-class Janta extends Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-      comida: props.comida
-    };
-    var comidas = ['Pizza', 'Lasanha', 'Burguer', 'Sopa', 'Arroz'];
-
-    setInterval(() => {
-      this.setState(previousState => {
-        let n = Math.floor(Math.random() * comidas.length);
-        return {comida: comidas[n]};
-      });
-    }, 1000);
-  }
-
-  render() {
-
-    return (
-      <View>
-        <Text style={{textAlign:'center', fontWeight: 'bold', fontSize: 20, color: 'red'}}>Hoje você vai jantar: </Text>
-        <Text style={{textAlign: 'center', fontSize: 20}}>{this.state.comida}</Text>
-      </View>
-    );
-  }
-}
-
 export default class PrimeiroProjeto extends Component {
 
   render() {
 
     return (
-      <View style={{paddingTop: 30}}>
-        <Janta comida='Bolacha'/>
+      <View style={styles.padrao}>
+        <Text style={styles.azulGrande}>Este é o texto 1</Text>
+        <Text style={styles.vermelho}>Este é o texto 2</Text>
+        <Text style={[styles.azulGrande, styles.vermelho]}>Este é o texto 3</Text>
+        <Text style={[styles.vermelho, styles.azulGrande]}>Este é o texto 4</Text>
       </View>
     );
   }
@@ -44,7 +19,17 @@ export default class PrimeiroProjeto extends Component {
 }
 
 const styles = StyleSheet.create({
-  texto : {
-    fontSize: 30
+  padrao: {
+    paddingTop: 20,
+    backgroundColor: '#00FF00'
+  },
+  azulGrande: {
+    color: 'blue',
+    fontSize: 30,
+    textAlign: 'center',
+  },
+  vermelho: {
+    color: 'red',
+    fontSize: 15
   }
 });
