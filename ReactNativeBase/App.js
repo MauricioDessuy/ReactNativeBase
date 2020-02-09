d2ba2f2fb6a5656dd874a9620c838eafae779ca8
Maurício Dessuy <mauriciodessuy@hotmail.com>
2020-02-09 19:50:24 -0300

    created a calculator app

--- ReactNativeBase/App.js
+++ ReactNativeBase/App.js
@@ -2,44 +2,72 @@ import React, { Component } from 'react';
 
 import { Text, View, StyleSheet, Button, Image, TextInput, Alert, ImageBackground, TouchableOpacity } from 'react-native';
 
+class Botao extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state ={
+
+    };
+
+    let c = 1;
+    if (props.c) {
+      c = parseInt(props.c);
+    }
+    let bg = '#e0e0e0';
+    if (props.bg) {
+      bg = props.bg;
+    }
+    this.styles = StyleSheet.create({
+      area: {
+        flex: c,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: '#999999',
+        backgroundColor: bg       
+      },
+      text: {
+        fontSize: 18
+      }
+    });
+  }
+
+  render() {
+    return (
+      <TouchableOpacity style={this.styles.area}
+        onPress={this.props.onPress}>
+        <Text style={this.styles.text}>{this.props.n}</Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
 export default class PrimeiroProjeto extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      n: 0,
-      botao: 'VAI'
+      r: 0
     };
-    this.timer = null;
-    this.vai = this.vai.bind(this);
-    this.limpar = this.limpar.bind(this);
+    this.btm = this.btn.bind(this);
   }
 
-  vai() {
-    let state = this.state;
-    if (this.timer != null) {
-      clearInterval(this.timer);
-      this.timer = null;
-      state.botao = 'VAI';
-    } else {
-      this.timer = setInterval(() => {
-        let s = this.state;
-        s.n += 0.1;
-        this.setState(s);
-      }, 100);
-      state.botao = 'PARAR';
-    }
-    this.setState(state);
-  }
-
-  limpar() {
-    if (this.timer != null) {
-      clearInterval(this.timer);
-      this.timer = null;
-    }
+  btn(b) {
     let s = this.state;
-    s.n = 0;
-    s.botao = 'VAI';
+
+    if (b == 'C') {
+      s.r = 0;
+    } else if (b == '=') {
+      s.r = eval(s.r)
+    } else {
+      if (s.r == 0) {
+        s.r = b;
+      } else {
+        s.r += b;
+      }
+    }
+
     this.setState(s);
   }
 
@@ -47,17 +75,35 @@ export default class PrimeiroProjeto extends Component {
 
     return (
       <View style={styles.body}>
-        <Image source={require('./assets/relogio.png')} />
-        <Text style={styles.timer}>{this.state.n.toFixed(1)}</Text>
-        <View style={styles.botaoArea}>
-          <TouchableOpacity style={styles.botao}
-            onPress={this.vai}>
-            <Text style={styles.botaoText}>{this.state.botao}</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.botao}
-            onPress={this.limpar}>
-            <Text style={styles.botaoText}>LIMPAR</Text>
-          </TouchableOpacity>
+        <View style={styles.linha}>
+          <Text style={styles.res}>{this.state.r}</Text>
+        </View>
+        <View style={styles.linha}>
+          <Botao c='3' n='C' bg='#cccccc' onPress={() => {this.btn("C")}}/>
+          <Botao n='/' bg="#FD9536" onPress={() => {this.btn("/")}}/>
+        </View>
+        <View style={styles.linha}>
+          <Botao n='7' onPress={() => {this.btn("7")}}/>
+          <Botao n='8' onPress={() => {this.btn("8")}}/>
+          <Botao n='9' onPress={() => {this.btn("9")}}/>
+          <Botao n='*' bg="#FD9536" onPress={() => {this.btn("*")}}/>
+        </View>
+        <View style={styles.linha}>
+          <Botao n='4' onPress={() => {this.btn("4")}}/>
+          <Botao n='5' onPress={() => {this.btn("5")}}/>
+          <Botao n='6' onPress={() => {this.btn("6")}}/>
+          <Botao n='-' bg="#FD9536" onPress={() => {this.btn("-")}}/>
+        </View>
+        <View style={styles.linha}>
+          <Botao n='1' onPress={() => {this.btn("1")}}/>
+          <Botao n='2' onPress={() => {this.btn("2")}}/>
+          <Botao n='3' onPress={() => {this.btn("3")}}/>
+          <Botao n='+' bg="#FD9536" onPress={() => {this.btn("+")}}/>
+        </View>
+        <View style={styles.linha}>
+          <Botao c='2' n='0' onPress={() => {this.btn("0")}}/>
+          <Botao n='.' onPress={() => {this.btn(".")}}/>
+          <Botao n='=' bg="#FD9536" onPress={() => {this.btn("=")}}/>
         </View>
       </View>
     );
@@ -68,35 +114,17 @@ export default class PrimeiroProjeto extends Component {
 const styles = StyleSheet.create({
   body: {
     paddingTop: 30,
+    flex: 1
+  },
+  linha: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#2c1f30'
+    flexDirection: 'row'
   },
-  timer: {
-    color: '#baa07a',
-    fontSize: 80,
-    fontWeight: 'bold',
-    backgroundColor: 'transparent',
-    marginTop: -160
-  },
-  botaoArea: {
-    flexDirection: 'row',
-    height: 40,
-    marginTop: 80
-  },
-  botao: {
+  res: {
+    backgroundColor: '#000000',
+    color: '#ffffff',
+    fontSize: 50,
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#886532',
-    height: 40,
-    borderRadius: 5,
-    margin: 10
-  },
-  botaoText: {
-    fontSize: 17,
-    fontWeight: 'bold',
-    color: '#FFFFFF'
+    textAlign: 'right'
   }
 });
